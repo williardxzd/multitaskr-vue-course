5f794b1ff510e4320d2522c409c8f461ec48d7b8
Williard Baez <willy09154@gmail.com>
2022-08-11 17:31:42 -0700

    Total input fetch

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,40 @@
-<script>
-</script>
-
 <template>
-  <main>
-    Testing
-  </main>
+    <main>
+        <Total v-model="total" :subtotal="subtotal"></Total>
+        {{ total }}
+    </main>
 </template>
+
+<script>
+    import Total from './components/Total.vue';
+
+    export default {
+        components: {
+            Total
+        },
+
+        data() {
+            return {
+                total: 0,
+                subtotal: 10000
+            }
+        },
+
+        async created() {
+            let response = await fetch('totals.json')
+            let body = await response.json()
+
+            this.total = body.data.total
+        },
+
+        mounted() {
+
+        },
+
+        watch: {
+            total: function(newValue) {
+                console.log(newValue)
+            }
+        }
+    }
+</script>
---
+++ src/components/Total.vue
@@ -0,0 +1,37 @@
+<template>
+    <input type="number" v-model="total">
+</template>
+
+<script>
+    export default {
+        props: ['subtotal', 'modelValue'],
+
+        data() {
+            return {
+                total: this.modelValue,
+                discount: 0,
+                taxes: 0,
+            };
+        },
+
+        mounted() {
+            console.log(this.total)
+        },
+
+        watch: {
+            total: function (value) {
+                this.$emit('update:modelValue', value);
+            },
+        },
+
+        methods: {
+            getTotalAmount() {
+                return this.total + this.getSubtotalAmount();
+            },
+
+            getSubtotalAmount() {
+                return this.subtotal;
+            },
+        },
+    };
+</script>
