520f5e633a9366ee7775c162cdedc89cd26ba88c
Williard Baez <willy09154@gmail.com>
2022-08-16 17:48:38 -0700

    Bootstrap installed

--- src/App.vue
+++ src/App.vue
@@ -16,14 +16,63 @@
                 </tr>
             </tbody>
         </table>
+
+        <!-- Button trigger modal -->
+        <button
+            type="button"
+            class="btn btn-primary"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+        >
+            Launch demo modal
+        </button>
+
+        <!-- Modal -->
+        <div
+            class="modal fade"
+            id="exampleModal"
+            tabindex="-1"
+            aria-labelledby="exampleModalLabel"
+            aria-hidden="true"
+        >
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">
+                            Modal title
+                        </h5>
+                        <button
+                            type="button"
+                            class="btn-close"
+                            data-bs-dismiss="modal"
+                            aria-label="Close"
+                        ></button>
+                    </div>
+                    <div class="modal-body">...</div>
+                    <div class="modal-footer">
+                        <button
+                            type="button"
+                            class="btn btn-secondary"
+                            data-bs-dismiss="modal"
+                        >
+                            Close
+                        </button>
+                        <button type="button" class="btn btn-primary">
+                            Save changes
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
 </template>
 
 <script>
+import { Modal } from 'bootstrap';
 export default {
     data() {
         return {
-            pokemons: []
+            pokemons: [],
         };
     },
 
@@ -32,8 +81,8 @@ export default {
             let response = await fetch('https://pokeapi.co/api/v2/pokemon');
             let body = await response.json();
             this.pokemons = body.results;
-        } catch(e) {
-            console.error(e)
+        } catch (e) {
+            console.error(e);
         }
     },
 };
